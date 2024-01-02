7bb0386e2dcb795c290f86dd2a61bf7dd3bc5f05
xiung <steven19790906@gmail.com>
2024-01-02 23:27:27 +0800

    0102

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const fetch = require('node-fetch')
+const moment = require('moment')
 // const path = require('path')
 // const sqlite3 = require('sqlite3').verbose()
 // var dbFile = '../../../data/user.sqlite'
@@ -12,8 +13,18 @@ const fetch = require('node-fetch')
 
 router.get('/home', function (request, response) {
     if (request.session.loggedin) {
-        response.render('home')
-        response.end()
+
+        const url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=<APIKEY>&StationId=C0A9F0&WeatherElement=AirTemperature"
+        
+        fetch(url).then(res => res.json())
+        .then(data => {
+            var date = data.records.Station[0].ObsTime.DateTime
+            var temperature = data.records.Station[0].WeatherElement.AirTemperature
+
+            var d = moment(date).format('MM/DD HH:mm')
+
+            response.render('home', {temperature: temperature, date: d})
+        })
     } else {
         response.send('Please login to view this page!');
         response.end();
